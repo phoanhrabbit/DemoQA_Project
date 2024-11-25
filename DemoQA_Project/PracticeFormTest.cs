e43b5514f55fed12dd07e3a494143bdedbac28fb
Jenny Nguyen <37173746+phoanhrabbit@users.noreply.github.com>
2024-11-25 22:40:42 +0700

    Add project files.

---
+++ DemoQA_Project/PracticeFormTest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace DemoQA_Project
+{
+    public class PracticeFormTest
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://demoqa.com/automation-practice-form";
+        }
+
+        [Test]
+        public void VerifyUserCanSubmitFormSuccessfullyAfterFillingAllTheInfo()
+        {
+            Assert.Pass();
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Dispose();
+        }
+    }
+}
