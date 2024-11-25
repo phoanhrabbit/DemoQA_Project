399604e8462cfa59e8094cd8f1ff2229334b42b4
Jenny Nguyen <37173746+phoanhrabbit@users.noreply.github.com>
2024-11-25 23:32:10 +0700

    add BasePage

---
+++ DemoQA_Project/Page/BasePage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace DemoQA_Project.Page
+{
+    public class BasePage
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Dispose();
+        }
+    }
+}
--- DemoQA_Project/Test/PracticeFormTest.cs
+++ DemoQA_Project/Test/PracticeFormTest.cs
@@ -1,37 +1,22 @@
 ﻿using DemoQA_Project.Page;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Runtime.Serialization;
 using WebDriverManager.DriverConfigs.Impl;
 
 namespace DemoQA_Project
 {
-    public class PracticeFormTest
+    public class PracticeFormTest: BasePage
     {
-        IWebDriver driver;
-        private PracticeFormPage practiceFormPage;
-
-        [SetUp]
-        public void Setup()
-        {
-            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
-            driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Url = "https://demoqa.com/automation-practice-form";
-            practiceFormPage = new PracticeFormPage(driver);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-        }
 
         [Test]
         public void VerifyUserCanSubmitFormSuccessfullyAfterFillingAllTheInfo()
         {
+            driver.Navigate().GoToUrl("https://demoqa.com/automation-practice-form");
+            var practiceFormPage = new PracticeFormPage(driver);
             practiceFormPage.sendFirstName("Oanh");
             TestContext.WriteLine("Passed");
             Assert.Pass();
-        }
-        [TearDown]
-        public void CloseBrowser()
-        {
-            driver.Dispose();
         }
     }
 }
