979dff69897d68fde2537a5fa318c11d25614c60
Phuong Oanh Nguyen Thi <nhokk0509@gmail.com>
2024-11-27 20:53:25 +0700

    update fill form

--- DemoQA_Project/Page/PracticeFormPage.cs
+++ DemoQA_Project/Page/PracticeFormPage.cs
@@ -30,20 +30,25 @@ namespace DemoQA_Project.Page
         //Month field
         private IWebElement monthField => driver.FindElement(By.XPath("//option[text()='September']"));
         //Date field
-        private IWebElement dateField => driver.FindElement(By.XPath("//div[@class='react-datepicker__week']/[7]"));
+        private IWebElement dateField => driver.FindElement(By.XPath("//div[@class='react-datepicker__week'][7]"));
         private IWebElement subjectField => driver.FindElement(By.XPath("//input[@id='subjectsInput']"));
         // option Sports
         private IWebElement hobbiesSportsField => driver.FindElement(By.CssSelector("#hobbies-checkbox-1"));
         // option Music
         private IWebElement hobbiesMusicField => driver.FindElement(By.CssSelector("#hobbies-checkbox-3"));
         private IWebElement FileField => driver.FindElement(By.CssSelector("#uploadPicture"));
+        private IWebElement currentAddressField => driver.FindElement(By.CssSelector("#currentAddress"));
+
+
 
 
         #endregion
         public void FillOutInformation(string firstName, string lastName, string subject)
         {
+            // enter first name + last name
             firstNameField.SendKeys(firstName);
             lastNameField.SendKeys(lastName);
+            // select date of birth
             dateOfBirthField.Click();
             yearField.Click();
             monthField.Click();
@@ -51,10 +56,14 @@ namespace DemoQA_Project.Page
             //click out the date time picker
             IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
             js.ExecuteScript("document.body.click();");
+            //enter subject
             subjectField.SendKeys(subject);
+            //select hobbies
             hobbiesSportsField.Click();
             hobbiesMusicField.Click();
+            //upload file
             FileField.SendKeys(Environment.CurrentDirectory.Replace(@"\", @"/") + "/File/File/auto.png");
+            //enter address
 
 
 
